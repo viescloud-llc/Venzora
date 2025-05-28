7a2cf4736b3ebf43424b69759b3305312491503c
VincentB00 <vincentbui1999@gmail.com>
2025-05-27 21:09:59 -0400

    feat: initial project setup with Spring Boot

---
+++ src/main/java/com/viescloud/llc/venzora/VenzoraApplication.java
@@ -0,0 +1,13 @@
+package com.viescloud.llc.venzora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenzoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenzoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viescloud/llc/venzora/VenzoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viescloud.llc.venzora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenzoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

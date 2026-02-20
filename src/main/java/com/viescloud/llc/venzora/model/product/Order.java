b33ba9469602e99f9e176230c59b102cf4ddf1ea
VincentB00 <vincentbui1999@gmail.com>
2026-02-20 16:01:50 -0500

    update order table

--- src/main/java/com/viescloud/llc/venzora/model/product/Order.java
+++ src/main/java/com/viescloud/llc/venzora/model/product/Order.java
@@ -8,6 +8,8 @@ import com.viescloud.eco.viesspringutils.model.TrackedTimeStampUserAccess;
 import com.viescloud.llc.venzora.model.address.Address;
 import com.viescloud.llc.venzora.model.product.type.OrderStatus;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
@@ -66,9 +68,27 @@ public class Order extends TrackedTimeStampUserAccess {
     private OrderStatus status;
 
     @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "street", column = @Column(name = "shipping_street", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "suite", column = @Column(name = "shipping_suite", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "city", column = @Column(name = "shipping_city", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "state", column = @Column(name = "shipping_state", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "postalCode", column = @Column(name = "shipping_postal_code", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "country", column = @Column(name = "shipping_country", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "type", column = @Column(name = "shipping_type"))
+    })
     private Address shippingAddress;
 
     @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "street", column = @Column(name = "billing_street", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "suite", column = @Column(name = "billing_suite", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "city", column = @Column(name = "billing_city", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "state", column = @Column(name = "billing_state", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "postalCode", column = @Column(name = "billing_postal_code", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "country", column = @Column(name = "billing_country", columnDefinition = "TEXT")),
+        @AttributeOverride(name = "type", column = @Column(name = "billing_type"))
+    })
     private Address billingAddress;
 
     @Column(columnDefinition = "TEXT")

2da7b51078a7f26270731792621c9fc9705fa30e
VincentB00 <vincentbui1999@gmail.com>
2026-02-20 14:50:31 -0500

    update bean config

--- src/main/java/com/viescloud/llc/venzora/config/BeanConfig.java
+++ src/main/java/com/viescloud/llc/venzora/config/BeanConfig.java
@@ -1,8 +1,21 @@
 package com.viescloud.llc.venzora.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.viescloud.eco.viesspringutils.config.ViesMinimalBeanConfig;
 
 @Configuration
 public class BeanConfig {
-    
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        return ViesMinimalBeanConfig.defaultCorsWebFilter();
+    }
+
+    @Bean
+    public WebMvcConfigurer CORSConfigurer() {
+        return ViesMinimalBeanConfig.defaultCORSConfigurer();
+    }
 }
